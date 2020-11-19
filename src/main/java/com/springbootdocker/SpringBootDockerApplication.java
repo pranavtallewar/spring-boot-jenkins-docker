b6bc4d237f1e6c9230f349ba44508830a0d2bd06
Pranav <pranav>
2020-11-19 20:30:09 +0530

    first commit

---
+++ src/main/java/com/springbootdocker/SpringBootDockerApplication.java
@@ -0,0 +1,20 @@
+package com.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+	@GetMapping
+	public String get() {
+		return "Hello from Spring Boot";
+	}
+}
