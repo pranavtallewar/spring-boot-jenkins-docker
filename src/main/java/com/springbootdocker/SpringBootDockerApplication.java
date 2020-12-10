9d841139ff42688e0d144cc13a57cf91ed3c60c6
Pranav <pranav>
2020-12-10 17:08:22 +0530

    second commit

--- src/main/java/com/springbootdocker/SpringBootDockerApplication.java
+++ src/main/java/com/springbootdocker/SpringBootDockerApplication.java
@@ -3,6 +3,7 @@ package com.springbootdocker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,5 +17,9 @@ public class SpringBootDockerApplication {
 	@GetMapping
 	public String get() {
 		return "Hello from Spring Boot";
+	}
+	@GetMapping("/hello/{name}")
+	public String getHello(@PathVariable("name") String name) {
+		return "Hello "+ name;
 	}
 }
